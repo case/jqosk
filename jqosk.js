716fc5ce597be3106901f9cb985ab18be7e70d8f
mistermuckle <mistermuckle@b96a271e-244a-0410-8c32-61de863e0274>
2008-04-08 15:17:57 +0000

    started filling in jqosk.js

--- jqosk.js
+++ jqosk.js
@@ -1,6 +1,43 @@
-(function($){
+(function() {
+
+function getJSONData(url) {
+  var retVal;
 
 
+}
 
+function OSK() {  
+  var shift = false;
+  this.shift = function(setting) {
+    if (arguments.length) {
+      return shift = !!setting;
+    } else {
+      return shift;
+    }
+  };
+  
+  var altGr = false;
+  this.altGr = function(setting) {
+    if (arguments.length) {
+      return shift = !!setting;
+    } else {
+      return shift;
+    }
+  };   
+};
 
-})(jQuery);
+OSK.prototype.loadLayout(layoutURL, forceReload) {
+  forceReload = forceReload || false;
+  if (forceReload || !this.layout) {
+    this.layout = getJSONData(layoutURL);
+    if (this.layout) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+window.jQOSK = new OSK;
+
+})();
